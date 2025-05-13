1c939bf46f3d211d40553704cce411b591dd593a
Nicolas Lamberto <nicolaslamberto2840@gmail.com>
2025-05-13 14:04:41 -0300

    Fix: Cloudinary provider and cleanup dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,15 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: '@strapi/provider-upload-cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
