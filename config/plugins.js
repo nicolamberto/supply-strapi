99f0e99b80a2bb641e2ee4cd8965d62b525c2375
Nicolas Lamberto <nicolaslamberto2840@gmail.com>
2025-05-13 14:21:55 -0300

    fixeando mas errores

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,21 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: '@strapi/provider-upload-cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+    'users-permissions': {
+        config: {
+            jwtSecret: env('JWT_SECRET', 'claveSuperSecretaDePrueba123!'),
+        },
     },
-  },
+    upload: {
+        config: {
+            provider: '@strapi/provider-upload-cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                delete: {},
+            },
+        },
+    },
 });
