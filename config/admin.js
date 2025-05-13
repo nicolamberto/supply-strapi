d4d734e8ee727c95c0d83c5a98112b59008d42f3
Nicolas Lamberto <nicolaslamberto2840@gmail.com>
2025-05-13 14:33:40 -0300

    error infinitos

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'test-admin-jwt-secret'), // Solo para pruebas
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'test-api-token-salt'), // Solo para pruebas
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'test-transfer-token-salt'), // Solo para pruebas
     },
   },
   flags: {
