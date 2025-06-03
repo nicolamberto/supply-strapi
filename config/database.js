c9300151086fe55f20603e3781574f80e7cb72ab
nicolamberto <nicolaslamberto2840@gmail.com>
2025-06-03 11:07:32 -0300

    migracion de sqlite a postgres

--- config/database.js
+++ config/database.js
@@ -8,9 +8,9 @@ module.exports = ({ env }) => {
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
