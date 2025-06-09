04bce2a2c20639e103f05162329589ec9ade79b4
nicolamberto <nicolaslamberto2840@gmail.com>
2025-06-09 04:02:03 -0300

    agregando cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('do9uhdawv'),
+        api_key: env('171531986578154'),
+        api_secret: env('gWXUTRr1aZicGHrH8M21BXLQ24E'),
+      },
+    },
+  },
+});
